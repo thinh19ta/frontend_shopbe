9dd39656817b4efd90b38f50e8dff6f7b9c6113e
thinhta <quocthinh1992002@gmail.com>
2024-02-22 11:09:18 +0700

    structure adjust - npm add boostrap@5, axios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import ListProduct from './components/ListProduct';
-import HomePage from './components/HomePage';
+import Test from './components/Test';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      {/* <ListProduct /> */}
-      <HomePage />
+    <div>
+      <Test />
     </div>
   );
 }
