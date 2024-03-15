b17cf3f1242f575f6d6eab866a66a2321c7f81da
thinhta <quocthinh1992002@gmail.com>
2024-03-15 14:05:45 +0700

    add assets folder + import bootstrap + style to app.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Home from './components/general/Home';
 import AboutUs from './components/general/AboutUs';
 import Shop from './components/shop/Shop';
 import ProductDetail from './components/shop/ProductDetail';
-
+import './assets/styles/style.css'
+import 'bootstrap'
 function App() {
   return (
     <div id="main-wrapper" className="section">
       <BrowserRouter>
+        askd
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/shop' element={<Shop />} />
